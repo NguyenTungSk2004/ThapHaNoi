20449b000c7414db61482a4b60af56f83627f3b0
NguyenTungSk2004 <nguyentungsk2004@gmail.com>
2023-10-31 21:37:22 +0700

    update logic game

--- main.py
+++ main.py
@@ -15,12 +15,92 @@ stack1_value = {}
 stack2_value = {}
 stack3_value = {}
 
-def checkTopRect(rect, stack, stack_value):
-    for i in range(len(stack)): # Duyệt qua các hình chữ nhật trong stack
-        if stack_value[stack[i]].y < rect.y: # Nếu có hình chữ nhật nào có vị trí y nhỏ hơn vị trí y của hình chữ nhật hiện tại
-            return False
-    return True
+# khung va chạm các cột
+collide1 = pygame.Rect(165,70,90,280)
+collide2 = pygame.Rect(540,70,90,280)
+collide3 = pygame.Rect(915,70,90,280)
 
+# Hàm check hình chữ nhật cao nhất trong stack
+def checkTopRect(rect):
+    def check(stack, stack_value):
+        for i in range(len(stack)): # Duyệt qua các hình chữ nhật trong stack
+            if stack_value[stack[i]].colliderect(rect): 
+                if i == len(stack)-1:
+                    return True
+        return False
+    
+    result = check(stack1,stack1_value)
+    if result:
+        return True
+    result = check(stack2,stack2_value)
+    if result:
+        return True
+    result = check(stack3,stack3_value)
+    if result:
+        return True
+    return False
+
+# Hàm check va chạm
+def checkCollide(rect):
+    def check(stack, stack_value):
+        for i in stack:
+            if stack_value[i].colliderect(rect):
+                return False
+        return True
+
+    if check(stack1, stack1_value) and rect.colliderect(collide1):
+        if not stack1:
+            return 1
+        elif stack1_value[stack1[len(stack1)-1]].width > rect.width or not stack1:
+            return 1
+    if check(stack2, stack2_value) and rect.colliderect(collide2):
+        if not stack2:
+            return 2
+        elif stack2_value[stack2[len(stack2)-1]].width > rect.width or not stack2:
+            return 2
+    if check(stack3, stack3_value) and rect.colliderect(collide3):
+        if not stack3:
+            return 3
+        elif stack3_value[stack3[len(stack3)-1]].width > rect.width:
+            return 3
+    return -1
+
+def checkInStack(rect,stack,stack_value,cot):
+    def process_stack(stackps,stackps_value):
+        for i in range(len(stackps)):
+            if stackps_value[stackps[i]].colliderect(rect): # Kiểm tra có đối tượng nào là rect hay không
+                # Thêm rect vào stack va chạm
+                stack.append(stackps[i]) 
+                stack_value[stackps[i]] = stackps_value[stackps[i]]
+                # Xóa đối tượng rect trong stack chứa nó
+                stackps_value.pop(stackps[i])
+                stackps.pop(i)
+                return True
+        return False
+    if stack != stack1:
+        # Kiểm tra xem rect có nằm trong stack1 không
+        if process_stack(stack1,stack1_value):
+            length = len(stack)
+            test = stack_value[stack[length-1]]
+            test.x = cot[0] - test.width/2 +10
+            test.y = cot[1] - (length-1)*30
+            return 
+    if stack != stack2:
+        # Kiểm tra xem rect có nằm trong stack2 không
+        if process_stack(stack2,stack2_value):
+            length = len(stack)
+            test = stack_value[stack[length-1]]
+            test.x = cot[0] - test.width/2 +10
+            test.y = cot[1] - (length-1)*30
+            return 
+    if stack != stack3:
+        # Kiểm tra xem rect có nằm trong stack3 không
+        if process_stack(stack3,stack3_value):
+            length = len(stack)
+            test = stack_value[stack[length-1]]
+            test.x = cot[0] - test.width/2 +10
+            test.y = cot[1] - (length-1)*30
+            return 
 class DrawGame:
 
     def __init__(self, surface):
@@ -30,6 +110,7 @@ class DrawGame:
         cot1 = pygame.Rect(200,200,20,400)
         cot2 = pygame.Rect(575,200,20,400)
         cot3 = pygame.Rect(950,200,20,400)
+      
         pygame.draw.rect(self.surface,(255, 0,0),cot1)
         pygame.draw.rect(self.surface,(255, 0,0),cot2)
         pygame.draw.rect(self.surface,(255, 0,0),cot3)
@@ -40,16 +121,17 @@ class DrawGame:
 class Player:
     def __init__(self, rect):
         self.rect = rect
+        self.original_pos = (rect.x, rect.y) # Lưu vị trí ban đầu của đĩa
         self.dragging = False # Biến để kiểm tra xem có đang kéo hình chữ nhật hay không
         self.offset = (0, 0) # Biến để lưu khoảng cách giữa con trỏ chuột và góc trên bên trái của hình chữ nhật
 
-    def handle_events(self, event):
+    def handle_events(self, event,x,y):
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1: # Nếu nhấn chuột trái
                 rect = self.rect
                 if rect.collidepoint(event.pos): # Nếu con trỏ chuột nằm trong hình chữ nhật
                     # Kiểm tra xem hình chữ nhật có phải là hình chữ nhật trên cùng của một cột hay không
-                    top_rect = checkTopRect(rect,stack1,stack1_value) 
+                    top_rect = checkTopRect(rect) 
                     if top_rect: # Nếu hình chữ nhật là hình chữ nhật trên cùng của một cột
                         self.dragging = True # Thiết lập dragging là True
                         mouse_x, mouse_y = event.pos # Lấy vị trí của con trỏ chuột
@@ -57,6 +139,18 @@ class Player:
         elif event.type == pygame.MOUSEBUTTONUP:
             if event.button == 1: # Nếu thả chuột trái
                 self.dragging = False # Thiết lập dragging là False
+                if checkCollide(self.rect) == 1:
+                    checkInStack(self.rect,stack1, stack1_value,(200,570))
+                    self.original_pos = (self.rect.x, self.rect.y) # Lưu vị trí ban đầu của đĩa
+                elif checkCollide(self.rect) == 2:
+                    checkInStack(self.rect,stack2, stack2_value,(575,570))
+                    self.original_pos = (self.rect.x, self.rect.y) # Lưu vị trí ban đầu của đĩa
+                elif checkCollide(self.rect) == 3:
+                    checkInStack(self.rect,stack3, stack3_value,(950,570))
+                    self.original_pos = (self.rect.x, self.rect.y) # Lưu vị trí ban đầu của đĩa
+                elif checkCollide(self.rect) == -1:
+                    self.rect.x = self.original_pos[0] # Di chuyển đĩa về vị trí ban đầu
+                    self.rect.y = self.original_pos[1] # Di chuyển đĩa về vị trí ban đầu
         elif event.type == pygame.MOUSEMOTION:
             if self.dragging: # Nếu đang kéo hình chữ nhật
                 mouse_x, mouse_y = event.pos # Lấy vị trí của con trỏ chuột
@@ -68,31 +162,33 @@ def createPlayer(n):
     players = [] # Tạo một danh sách để lưu các đối tượng player
     for i in range(n):
         layer = pygame.Rect(180-i*r/2,570-(n-i-1)*r,60+i*r,r)
-        stack1.append(str(i))
-        stack1_value[stack1[i]] = layer
-        players.append(Player(layer)) # Tạo một đối tượng player cho mỗi hình chữ nhật và thêm vào danh sách players
+        stack1.insert(0, str(i))
+        stack1_value[stack1[len(stack1)-1-i]] = layer
+        players.insert(0, Player(layer)) # Tạo một đối tượng player cho mỗi hình chữ nhật và thêm vào danh sách players
     return players # Trả về danh sách players
 
 def process(event, players):
     for player in players: # Duyệt qua các đối tượng player trong danh sách players
-        player.handle_events(event) # Gọi phương thức handle_events cho mỗi đối tượng player
+        player.handle_events(event,player.rect.x,player.rect.y) # Gọi phương thức handle_events cho mỗi đối tượng player
 
 draw = DrawGame(screen)
 running = True
-players = createPlayer(10) # Gọi hàm createPlayer và lưu kết quả vào biến players
+players = createPlayer(4) # Gọi hàm createPlayer và lưu kết quả vào biến players
 while running:
+    # Phần vẽ
     screen.fill((255, 255, 255))
     draw.draw_columns()
-    length = len(stack1)
-    for i in range(length):
-        draw.draw(stack1_value[stack1[i]])
-    
+    for i in players:
+        draw.draw(i)
+    # Phần xử lí sự kiện
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         else: 
             process(event, players) # Gọi hàm process với biến players
-
+    if len(stack3) == 4:
+        print("ban da chien thang")
+        running = False
     pygame.display.update()
     clock.tick(40)
 
--- test.py
+++ test.py
@@ -1,91 +1,9 @@
-import pygame
-
-pygame.init()
-screen = pygame.display.set_mode((1200,600))
-pygame.display.set_caption("Tháp Hà Nội")
-clock = pygame.time.Clock()
-
-# các stack chứa nhãn của đĩa
-stack1 =[]
-stack2 =[]
-stack3 =[]
-
-# giá trị của đĩa
-stack1_value = {}
-stack2_value = {}
-stack3_value = {}
-
-class DrawGame:
-
-    def __init__(self, surface):
-        self.surface = surface 
+stack = [1,2,3,4,5]
+stack1 = [1,2,3,4]
+def check(s):
+    if s != stack:
+        return "stack1"
+    if s != stack1:
+        return "stack"
     
-    def draw_columns(self):
-        cot1 = pygame.Rect(200,200,20,400)
-        cot2 = pygame.Rect(575,200,20,400)
-        cot3 = pygame.Rect(950,200,20,400)
-        pygame.draw.rect(self.surface,(255, 0,0),cot1)
-        pygame.draw.rect(self.surface,(255, 0,0),cot2)
-        pygame.draw.rect(self.surface,(255, 0,0),cot3)
-
-    def draw(self, rect):
-        pygame.draw.rect(self.surface, (0, 0, 128), rect)
-    
-class Player:
-    def __init__(self, rect):
-        self.rect = rect
-        self.dragging = False # Biến để kiểm tra xem có đang kéo hình chữ nhật hay không
-        self.offset = (0, 0) # Biến để lưu khoảng cách giữa con trỏ chuột và góc trên bên trái của hình chữ nhật
-
-    def handle_events(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1: # Nếu nhấn chuột trái
-                rect = self.rect
-                if rect.collidepoint(event.pos): # Nếu con trỏ chuột nằm trong hình chữ nhật
-                    self.dragging = True # Thiết lập dragging là True
-                    mouse_x, mouse_y = event.pos # Lấy vị trí của con trỏ chuột
-                    self.offset = (rect.x - mouse_x, rect.y - mouse_y) # Tính toán offset
-        elif event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1: # Nếu thả chuột trái
-                self.dragging = False # Thiết lập dragging là False
-        elif event.type == pygame.MOUSEMOTION:
-            if self.dragging: # Nếu đang kéo hình chữ nhật
-                mouse_x, mouse_y = event.pos # Lấy vị trí của con trỏ chuột
-                self.rect.x = mouse_x + self.offset[0] # Cập nhật vị trí x của hình chữ nhật bằng cách cộng offset với vị trí x của chuột
-                self.rect.y = mouse_y + self.offset[1] # Cập nhật vị trí y của hình chữ nhật bằng cách cộng offset với vị trí y của chuột
-
-def createPlayer(n):
-    r = 30
-    # layer = [(180, 570, 60, 30)]
-    for i in range(n):
-        layer = pygame.Rect(180-i*r/2,570-(n-i-1)*r,60+i*r,r)
-        stack1.append(str(i))
-        stack1_value[stack1[i]] = layer
-
-def process(event):
-    if stack1:
-        player = Player(stack1_value[stack1[0]])
-        player.handle_events(event)
-
-draw = DrawGame(screen)
-running = True
-createPlayer(10)
-print(stack1)
-print(stack1_value)
-while running:
-    screen.fill((255, 255, 255))
-    draw.draw_columns()
-    length = len(stack1)
-    for i in range(length):
-        draw.draw(stack1_value[stack1[i]])
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        else: 
-            process(event)
-
-    pygame.display.update()
-    clock.tick(40)
-
-pygame.quit()
+print(check(stack1))
